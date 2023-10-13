a9003a06da6d55924bdc139bc2fc9c1caa730d41
Roman <31464179+grand-roman@users.noreply.github.com>
2023-10-13 22:01:27 +0700

    add profile

--- FakeNFT/Scenes /TabBar/TabBarController.swift
+++ FakeNFT/Scenes /TabBar/TabBarController.swift
@@ -19,5 +19,39 @@ final class TabBarController: UITabBarController {
         UITabBarItem.appearance().setTitleTextAttributes(
             [NSAttributedString.Key.font: UIFont.Medium.size10], for: .selected
         )
+
+        let profileVC = UINavigationController(rootViewController: ProfileViewController())
+        profileVC.tabBarItem = UITabBarItem(
+            title: .TabBar.profile,
+            image: .TabBar.profile,
+            selectedImage: nil
+        )
+
+        let catalogVC = UINavigationController(rootViewController: CatalogViewController())
+        catalogVC.tabBarItem = UITabBarItem(
+            title: .TabBar.catalog,
+            image: .TabBar.catalog,
+            selectedImage: nil
+        )
+
+        let cartVC = UINavigationController(rootViewController: CartViewController())
+        cartVC.tabBarItem = UITabBarItem(
+            title: .TabBar.cart,
+            image: .TabBar.cart,
+            selectedImage: nil
+        )
+
+        let statisticsVC = UINavigationController(rootViewController: StatisticsViewController())
+        statisticsVC.tabBarItem = UITabBarItem(
+            title: .TabBar.statistics,
+            image: .TabBar.statistics,
+            selectedImage: nil
+        )
+
+        [profileVC, catalogVC, cartVC, statisticsVC].forEach {
+            $0.view.backgroundColor = .Themed.white
+        }
+
+        viewControllers = [profileVC, catalogVC, cartVC, statisticsVC]
     }
 }
